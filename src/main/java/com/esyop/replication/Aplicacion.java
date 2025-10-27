d473f159678e2c27179385ac4c6f7aa8a4205380
Rafael Alvarez <rafaelrio4@gmail.com>
2025-10-27 11:46:54 -0300

    Commit final

--- src/main/java/com/esyop/replication/Aplicacion.java
+++ src/main/java/com/esyop/replication/Aplicacion.java
@@ -2,12 +2,16 @@ package com.esyop.replication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+
 
 @SpringBootApplication
 @EnableScheduling
 public class Aplicacion {
-    public static void main(String[] args) {
+
+      public static void main(String[] args) {
         SpringApplication.run(Aplicacion.class, args);
     }
 }
